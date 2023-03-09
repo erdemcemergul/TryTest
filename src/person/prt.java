c139a3c027cfa4980cf3c1401427e1041178ae8f
erdemcemergul <erdemcemergul@gmail.com>
2023-03-09 22:37:00 +0300

    jokerin dosyası eklendi

--- src/person/prt.java
+++ src/person/prt.java
@@ -3,6 +3,8 @@ package person;
 public class prt {
     public static void main(String[] args) {
        Employee personal =new TesterECE();
+      Employee pers=new Tester_Joker();
+      pers.name();
        personal.name();
     }
 }
