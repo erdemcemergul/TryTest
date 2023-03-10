c034a27dd09a01a43e464437654569de90286cd1
Ali Can <81786704+AliCan2023@users.noreply.github.com>
2023-03-10 14:54:41 +0300

    ALi Finished the Job

--- src/person/TestACB.java
+++ src/person/TestACB.java
@@ -3,6 +3,6 @@ package person;
 public class TestACB extends Employee{
     public void name() {
 
-        System.out.println("Ali Can Bugraoglu");
+        System.out.println("Ali Can Buğraoğlu");
     }
 }
