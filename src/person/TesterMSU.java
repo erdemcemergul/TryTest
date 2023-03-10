5cce1813d96549bea4f97e69f995ed961b4e17ab
ms-joker <joker728@gmail.com>
2023-03-10 22:10:54 +0300

    Mehmet Siddik Added New File

---
+++ src/person/TesterMSU.java
@@ -0,0 +1,12 @@
+package person;
+
+public class TesterMSU extends Employee{
+
+    public static void main(String[] args) {
+        System.out.println("I am Mehmet Siddik.");
+    }
+    @Override
+    public void name() {
+        System.out.println("Tester Mehmet Siddik UYGUROGLU");
+    }
+}
