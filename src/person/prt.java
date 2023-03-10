6b87e479b70aca8051d3666addad4e881dad2278
erdemcemergul <112733324+erdemcemergul@users.noreply.github.com>
2023-03-10 17:24:09 +0300

    Update prt.java

--- src/person/prt.java
+++ src/person/prt.java
@@ -4,9 +4,11 @@ public class prt {
     public static void main(String[] args) {
        Employee personal =new TesterECE();
        Employee personal2=new TestACB();
-       Employee personal3 =new TesterOY();
+       Employee personal3=new TesterOY();
+       Employee personal4=new TesterBY();
        personal.name();
        personal2.name();
        personal3.name();
+       personal4.name();
     }
 }
