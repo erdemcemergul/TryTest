0901ed1e76462a4d88caf3d7f3298e295704bbc1
erdemcemergul <112733324+erdemcemergul@users.noreply.github.com>
2023-03-10 17:13:46 +0300

    Update prt.java
    update for OY

--- src/person/prt.java
+++ src/person/prt.java
@@ -4,7 +4,9 @@ public class prt {
     public static void main(String[] args) {
        Employee personal =new TesterECE();
        Employee personal2=new TestACB();
+       Employee personal3 =new TesterOY();
        personal.name();
        personal2.name();
+       personal3.name();
     }
 }
