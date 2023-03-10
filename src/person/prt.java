76fba94d0ff4c493aa13a52b7d6ef5a9e3402d40
erdemcemergul <112733324+erdemcemergul@users.noreply.github.com>
2023-03-10 15:25:56 +0300

    Update prt.java
    TestACB eklendi

--- src/person/prt.java
+++ src/person/prt.java
@@ -3,6 +3,8 @@ package person;
 public class prt {
     public static void main(String[] args) {
        Employee personal =new TesterECE();
+       Employee personal2=new TestACB();
        personal.name();
+       personal2.name();
     }
 }
